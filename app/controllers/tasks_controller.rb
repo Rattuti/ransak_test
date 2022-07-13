7223449a569fe5c7f7d00a7ebfcd76efd029b445
Rattuti <i.ishiharar.96296@gmail.com>
2022-07-13 15:21:42 +0900

    first

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :set_q, only: [:index, :search]
 
   # GET /tasks
   def index
-    @tasks = Task.all
+    @q = Task.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
   end
 
   # GET /tasks/1
@@ -46,15 +46,7 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: 'Task was successfully destroyed.'
   end
 
-  def search
-    @results = @q.result
-  end
-
   private
-    def set_q
-      @q = Task.ransack(params[:q])
-    end
-
     # Use callbacks to share common setup or constraints between actions.
     def set_task
       @task = Task.find(params[:id])
